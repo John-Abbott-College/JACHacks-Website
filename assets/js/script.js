7cf47283552e9ff5e4341db892eb16856f1e1456
Bogdan Feher <bogdan.feher@gmail.com>
2025-02-21 02:14:48 -0500

    removed unecessary images and readded typewritting cause its very nice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+let language = document.getElementsByTagName("html")[0].getAttribute("lang");
+if ("fr" == language) var aText = new Array("26-27 Avril");
+else aText = new Array("April 26-27");
+var iRow,
+  iSpeed = 140,
+  iIndex = 0,
+  iArrLength = aText[0].length,
+  iScrollAt = 20,
+  iTextPos = 0,
+  sContents = "";
+function typewriter() {
+  (sContents = " "), (iRow = Math.max(0, iIndex - iScrollAt));
+  for (var e = document.getElementById("typedtext"); iRow < iIndex; )
+    sContents += aText[iRow++] + "<br />";
+  (e.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_"),
+    iTextPos++ == iArrLength
+      ? ((iTextPos = 0),
+        ++iIndex != aText.length &&
+          ((iArrLength = aText[iIndex].length),
+          setTimeout("typewriter()", 500)))
+      : setTimeout("typewriter()", iSpeed);
+}
+typewriter();
+
 document.addEventListener("DOMContentLoaded", function () {
   const images = document.querySelectorAll(".pop-up-image");
 
